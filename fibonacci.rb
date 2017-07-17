cdf0cb302b5e177360f7b999dff1647de457ea0d
vnframe <vnframe95@icloud.com>
2017-07-17 14:35:35 -0400

    fib

--- fibonacci.rb
+++ fibonacci.rb
@@ -6,11 +6,11 @@ x = 1	# sets innitial values for x and y
 y = 2
 (1..n).each do
 	z = (x + y) # adds inital numbers and sets value for their sum
-	x = y # sets x and y to the last two numbers in the sequences
+	x = y # sets x and y to the last two numbers in the sequence
 	y = z
 end 
 	return y #returns last number
 	
 end
 p (0..30).collect {|n| fibonacci(n)} # sets n and calls in fibonnaci method
-				 # 30 is the lowest n can be set to for y < 4million
+				 # 30 is the lowest n can be set to for y =< 4million
